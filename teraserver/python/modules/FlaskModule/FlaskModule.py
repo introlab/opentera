10e39d460ddab3ab63b52c446688aec19a2dce49
Dominic Létourneau <doumdi@gmail.com>
2019-03-18 11:28:28 -0400

    index is not part of the REST API

--- teraserver/python/modules/FlaskModule/FlaskModule.py
+++ teraserver/python/modules/FlaskModule/FlaskModule.py
@@ -22,11 +22,14 @@ class FlaskModule:
         self.init_api()
 
     def init_api(self):
-        from .API.Index import Index
+        # from .API.Index import Index
         from .API.QueryUsers import QueryUsers
         from .API.Profile import Profile
-
-        # Index for testing...
-        self.api.add_resource(Index, '/')
         self.api.add_resource(QueryUsers, '/api/users')
         self.api.add_resource(Profile, '/api/profile')
+
+    # Index for testing...
+    @flask_app.route('/', methods=['GET', 'POST'])
+    @auth.login_required
+    def index():
+        return render_template('index.html')
