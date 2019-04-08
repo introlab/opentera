c46ad9594c2f7988d385b52fb97be3c03753169b
Dominic Létourneau <doumdi@gmail.com>
2019-04-08 16:29:03 -0400

    Added test devices

--- teraserver/python/libtera/db/models/TeraDevice.py
+++ teraserver/python/libtera/db/models/TeraDevice.py
@@ -1,17 +1,77 @@
 from libtera.db.Base import db, BaseModel
+from libtera.db.models.TeraDeviceType import TeraDeviceType
+import uuid
+import jwt
+import time
+import datetime
 
 
 class TeraDevice(db.Model, BaseModel):
     __tablename__ = 't_devices'
+    secret = 'TeraDeviceSecret'
     id_device = db.Column(db.Integer, db.Sequence('id_device_sequence'), primary_key=True, autoincrement=True)
     id_kit = db.Column(db.Integer, db.ForeignKey("t_kits.id_kit"), nullable=True)
     device_uuid = db.Column(db.String(36), nullable=False, unique=True)
     device_name = db.Column(db.String, nullable=False)
     device_type = db.Column(db.Integer, db.ForeignKey('t_devices_types.id_device_type'), nullable=False)
-    device_token = db.Column(db.String(36), nullable=False)
+    device_token = db.Column(db.String, nullable=False, unique=True)
     device_enabled = db.Column(db.Boolean, nullable=False)
     device_onlineable = db.Column(db.Boolean, nullable=False)
     device_profile = db.Column(db.String, nullable=True)
     device_notes = db.Column(db.String, nullable=True)
     device_lastonline = db.Column(db.TIMESTAMP, nullable=True)
 
+    def create_token(self):
+        # Creating token with user info
+        payload = {
+            'iat': int(time.time()),
+            'device_uuid': self.device_uuid,
+            'device_name': self.device_name,
+            'device_type': self.device_type
+        }
+
+        # TODO key should be secret ?
+        self.device_token = jwt.encode(payload, TeraDevice.secret, 'HS256').decode('utf-8')
+
+        return self.device_token
+
+    def update_last_online(self):
+        self.device_lastonline = datetime.datetime.now()
+        db.session.commit()
+
+    @staticmethod
+    def get_device_by_token(token):
+        device = TeraDevice.query.filter_by(device_token=token).first()
+
+        if device:
+            # Validate token
+            data = jwt.decode(token.encode('utf-8'), TeraDevice.secret, 'HS256')
+
+            if data['device_uuid'] == device.device_uuid \
+                    and data['device_name'] == device.device_name:
+
+                # Update last online
+                device.update_last_online()
+
+                return device
+            else:
+                return None
+
+        return None
+
+    @staticmethod
+    def create_defaults():
+        device = TeraDevice()
+        device.device_name = 'Test Device #1'
+        device.device_uuid = str(uuid.uuid4())
+        device.device_type = TeraDeviceType.DeviceTypeEnum.SENSOR.value
+        device.create_token()
+        device.device_enabled = True
+        device.device_onlineable = True
+        db.session.add(device)
+        db.session.commit()
+
+    @staticmethod
+    def get_count():
+        count = db.session.query(db.func.count(TeraDevice.id_device))
+        return count.first()[0]
--- teraserver/python/libtera/db/models/TeraDeviceType.py
+++ teraserver/python/libtera/db/models/TeraDeviceType.py
@@ -1,12 +1,33 @@
 from libtera.db.Base import db, BaseModel
-
-# class TeraDeviceTypes(Enum):
-#     VIDEOCONFERENCE = 1
-#     ROBOT = 2
-#     SENSOR = 3
+from enum import Enum, unique
 
 
 class TeraDeviceType(db.Model, BaseModel):
+
+    @unique
+    class DeviceTypeEnum(Enum):
+        VIDEOCONFERENCE = 1
+        ROBOT = 2
+        SENSOR = 3
+
+        def describe(self):
+            return self.name, self.value
+
     __tablename__ = 't_devices_types'
     id_device_type = db.Column(db.Integer, db.Sequence('id_device_sequence'), primary_key=True, autoincrement=True)
     device_type_name = db.Column(db.String, nullable=False)
+
+    @staticmethod
+    def create_defaults():
+
+        for name, member in TeraDeviceType.DeviceTypeEnum.__members__.items():
+            devType = TeraDeviceType()
+            devType.device_type_name = name
+            db.session.add(devType)
+
+        db.session.commit()
+
+    @staticmethod
+    def get_count():
+        count = db.session.query(db.func.count(TeraDeviceType.id_device_type))
+        return count.first()[0]
