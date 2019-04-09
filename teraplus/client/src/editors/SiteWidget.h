55681c433043e6f33d0ba85778fb6dd3b7402831
Simon Brière <simon.briere@usherbrooke.ca>
2019-04-09 15:37:13 -0400

    Fixed wrong version of SiteWidget.h in last commit.

--- teraplus/client/src/editors/SiteWidget.h
+++ teraplus/client/src/editors/SiteWidget.h
@@ -3,20 +3,24 @@
 
 #include <QWidget>
 
+#include "DataEditorWidget.h"
+
 namespace Ui {
 class SiteWidget;
 }
 
-class SiteWidget : public QWidget
+class SiteWidget : public DataEditorWidget
 {
     Q_OBJECT
 
 public:
-    explicit SiteWidget(QWidget *parent = nullptr);
+    explicit SiteWidget(ComManager* comMan, const TeraData* data = nullptr, QWidget *parent = nullptr);
     ~SiteWidget();
 
 private:
     Ui::SiteWidget *ui;
+
+    void connectSignals();
 };
 
 #endif // SITEWIDGET_H
