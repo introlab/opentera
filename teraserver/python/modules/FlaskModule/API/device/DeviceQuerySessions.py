f8c4cd2de777a4bf5d9cf0c92cda23db6a272665
Dominic Létourneau <doumdi@gmail.com>
2019-09-13 11:48:14 -0400

    working end sessions

--- teraserver/python/modules/FlaskModule/API/device/DeviceQuerySessions.py
+++ teraserver/python/modules/FlaskModule/API/device/DeviceQuerySessions.py
@@ -89,6 +89,10 @@ class DeviceQuerySessions(Resource):
             # Already existing
             # TODO handle participant list (remove, add) in session
             try:
+                if 'session_participants' in json_session:
+                    participants = json_session.pop('session_participants')
+                    print('removing participants', participants)
+
                 TeraSession.update(json_session['id_session'], json_session)
             except exc.SQLAlchemyError:
                 import sys
