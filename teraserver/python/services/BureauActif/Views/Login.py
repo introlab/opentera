f967a17fe66a1554a1e651388eea89e9fd6c6136
Dominic Létourneau <doumdi@gmail.com>
2020-01-13 10:55:36 -0500

    Printing server informatin in login page

--- teraserver/python/services/BureauActif/Views/Login.py
+++ teraserver/python/services/BureauActif/Views/Login.py
@@ -15,7 +15,10 @@ class Login(MethodView):
         print('get')
         hostname = self.flaskModule.config.server_config['hostname']
         port = self.flaskModule.config.server_config['port']
-        return render_template('login.html', hostname=hostname, port=port)
+        backend_hostname = self.flaskModule.config.backend_config['hostname']
+        backend_port = self.flaskModule.config.backend_config['port']
+        return render_template('login.html', hostname=hostname, port=port,
+                               backend_hostname=backend_hostname, backend_port=backend_port)
 
     def post(self):
         print('post')
