e4ad0c4d23d6cb03848eb04f0804ca21aea7e526
Dominic Létourneau <doumdi@gmail.com>
2020-02-27 12:10:19 -0500

    Fixed servername that contains also the port from nginx

--- teraserver/python/modules/FlaskModule/API/user/Login.py
+++ teraserver/python/modules/FlaskModule/API/user/Login.py
@@ -29,7 +29,10 @@ class Login(Resource):
         servername = self.module.config.server_config['hostname']
         port = self.module.config.server_config['port']
         if 'X_EXTERNALHOST' in request.headers:
-            servername = request.headers['X_EXTERNALHOST'];
+            if ':' in request.headers['X_EXTERNALHOST']:
+                servername, port = request.headers['X_EXTERNALHOST'].split(':', 1)
+            else:
+                servername, port = request.headers['X_EXTERNALHOST']
 
         if 'X_EXTERNALPORT' in request.headers:
             port = request.headers['X_EXTERNALPORT'];
