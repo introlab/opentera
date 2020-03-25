a31e0fcf3933b1810b8b4d6219449b3a11c23910
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2020-03-25 15:04:13 -0400

    Refs #25. First redirect to dashboard.

--- teraserver/python/services/VideoDispatch/Views/Login.py
+++ teraserver/python/services/VideoDispatch/Views/Login.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from flask import render_template, request
+from flask import render_template, request, redirect
 
 
 class Login(MethodView):
@@ -10,21 +10,30 @@ class Login(MethodView):
         self.flaskModule = kwargs.get('flaskModule', None)
 
     def get(self):
-        print('get')
         # Set variables for template
-        hostname = self.flaskModule.config.server_config['hostname']
-        port = self.flaskModule.config.server_config['port']
-        backend_hostname = self.flaskModule.config.backend_config['hostname']
-        backend_port = self.flaskModule.config.backend_config['port']
-        if 'X_EXTERNALHOST' in request.headers:
-            backend_hostname = request.headers['X_EXTERNALHOST']
+        if 'participant_token' in request.args:
+            # Create cookie
+            path = '/dashboard'
 
-        if 'X_EXTERNALPORT' in request.headers:
-            backend_port = request.headers['X_EXTERNALPORT']
+            if 'X-Script-Name' in request.headers:
+                path = request.headers['X-Script-Name'] + path
 
-        # Render page
-        return render_template('login.html', hostname=hostname, port=port,
-                               backend_hostname=backend_hostname, backend_port=backend_port)
+            # redirect to dashboard
+            return redirect(path)
+        else:
+            hostname = self.flaskModule.config.server_config['hostname']
+            port = self.flaskModule.config.server_config['port']
+            backend_hostname = self.flaskModule.config.backend_config['hostname']
+            backend_port = self.flaskModule.config.backend_config['port']
+            if 'X-Externalhost' in request.headers:
+                backend_hostname = request.headers['X-Externalhost']
+
+            if 'X-Externalport' in request.headers:
+                backend_port = request.headers['X-Externalport']
+
+            # Render page
+            return render_template('login.html', hostname=hostname, port=port,
+                                   backend_hostname=backend_hostname, backend_port=backend_port)
 
     def post(self):
         print('post')
