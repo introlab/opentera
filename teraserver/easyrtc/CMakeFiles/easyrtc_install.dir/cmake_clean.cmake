e5b99919e607f8dd54002feaf26dda9e594c2f65
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2020-04-01 09:23:58 -0400

    Removed unused files

--- teraserver/easyrtc/CMakeFiles/easyrtc_install.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/easyrtc_install"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/easyrtc_install.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
