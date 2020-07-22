88d25c90764ea71af58525af9bf951a5ecd2a0e3
Dominic Létourneau <doumdi@gmail.com>
2020-07-22 15:11:17 -0400

    Refs #41. Create FileTransferService skeleton.

---
+++ teraserver/python/services/FileTransferService/Globals.py
@@ -0,0 +1,18 @@
+from services.FileTransferService.ConfigManager import ConfigManager
+from services.FileTransferService.libfiletransferservice.db.DBManager import DBManager
+
+# Configuration manager
+config_man = ConfigManager()
+
+# Database manager
+db_man = DBManager()
+
+# Redis client & keys
+redis_client = None
+api_user_token_key = None
+api_device_token_key = None
+api_device_static_token_key = None
+api_participant_token_key = None
+api_participant_static_token_key = None
+
+
