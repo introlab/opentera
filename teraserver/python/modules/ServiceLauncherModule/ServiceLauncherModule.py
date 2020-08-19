375bfa822ea9a7569f1f1562ec40da8d8ad418ce
Dominic Létourneau <doumdi@gmail.com>
2020-08-19 14:41:33 -0400

    Refs #47 starting system services. Basic implementation.

--- teraserver/python/modules/ServiceLauncherModule/ServiceLauncherModule.py
+++ teraserver/python/modules/ServiceLauncherModule/ServiceLauncherModule.py
@@ -1,15 +1,29 @@
 from modules.BaseModule import BaseModule, ModuleNames
 from libtera.ConfigManager import ConfigManager
+from libtera.db.models.TeraService import TeraService
+
+import os
+import subprocess
+import threading
+import sys
 
 
 class ServiceLauncherModule(BaseModule):
 
     def __init__(self, config: ConfigManager):
         BaseModule.__init__(self, ModuleNames.SERVICE_LAUNCHER_NAME.value, config)
+        self.processList = []
 
     def setup_module_pubsub(self):
         # Additional subscribe here
-        pass
+
+        # Launch all internal services
+        services = TeraService.query.all()
+        for service in services:
+            if service.service_system:
+                print(service)
+                if service.service_key != 'OpenTeraServer':
+                    self.launch_service(service)
 
     def notify_module_messages(self, pattern, channel, message):
         """
@@ -20,3 +34,30 @@ class ServiceLauncherModule(BaseModule):
 
     def setup_rpc_interface(self):
         pass
+
+    def launch_service(self, service: TeraService):
+        print('Launching service: ', service.service_key)
+        # First argument will be python executable
+        executable_args = [sys.executable]
+        working_directory = os.getcwd()
+        # TODO Hardcoded paths for service right now
+        if service.service_key == 'LoggingService':
+            path = os.path.join(os.getcwd(), 'services', 'LoggingService', 'LoggingService.py')
+            executable_args.append(path)
+            working_directory = os.path.join(os.getcwd(), 'services', 'LoggingService')
+        elif service.service_key == 'FileTransferService':
+            path = os.path.join(os.getcwd(), 'services', 'FileTransferService', 'FileTransferService.py')
+            executable_args.append(path)
+            working_directory = os.path.join(os.getcwd(), 'services', 'FileTransferService')
+        else:
+            print('Unable to start :', service.service_key)
+            return
+
+        # Start process
+        process = subprocess.Popen(executable_args, cwd=os.path.realpath(working_directory))
+        process_dict = {
+            'process': process,
+            'service': service.to_json()
+        }
+        self.processList.append(process_dict)
+        print('ServiceLauncherModule.launch_service, service started:', process_dict)
