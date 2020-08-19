6d2557f5f45420919f2a1077e02ab01db0452eca
Dominic Létourneau <doumdi@gmail.com>
2020-08-19 15:52:14 -0400

    updated start script

--- teraserver/python/start_docker.sh
+++ teraserver/python/start_docker.sh
@@ -1,13 +1,12 @@
 echo "Starting postresql"
 service postgresql start
 echo "Starting redis-server"
-redis-server &
+service redis-server start
 echo "Sleeping 5 secs."
 sleep 5
 echo "Starting TeraServer"
-$PYTHON3_EXEC ./TeraServer.py &
-sleep 5
+$PYTHON3_EXEC ./TeraServer.py
+# sleep 5
 # echo "Starting VideoDispatchServer"
 # $PYTHON3_EXEC ./services/VideoDispatch/VideoDispatchService.py &
-# echo "Starting bash"
-bash
+
