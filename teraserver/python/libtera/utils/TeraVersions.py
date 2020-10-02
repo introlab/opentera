6b7314111527e26b874a25c3ad9a74d60d7b3cc2
Dominic Létourneau <doumdi@gmail.com>
2020-10-02 08:42:30 -0400

    Refs #48, Fixed error when loading from new db with missing field.

--- teraserver/python/libtera/utils/TeraVersions.py
+++ teraserver/python/libtera/utils/TeraVersions.py
@@ -162,7 +162,10 @@ class TeraVersions:
 
     def load_from_db(self):
         settings = TeraServerSettings.get_server_setting_value(TeraServerSettings.ServerVersions)
-        self.from_dict(json.loads(settings))
+        if settings:
+            self.from_dict(json.loads(settings))
+            return True
+        return False
 
     def __repr__(self):
         return '<TeraVersions: ' + json.dumps(self.to_dict()) + ' >'
