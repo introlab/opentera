3c9089ed05f6792d453a159d4038ffd3f72be9d1
Dominic Létourneau <doumdi@gmail.com>
2020-10-05 16:56:43 -0400

    Updated nginx script to take client certificates into account, test working

--- teraserver/python/modules/TwistedModule/TwistedModule.py
+++ teraserver/python/modules/TwistedModule/TwistedModule.py
@@ -41,13 +41,13 @@ class MyHTTPChannel(HTTPChannel):
         req = self.requests[-1]
 
         # SAFETY X-Device-UUID, X-Participant-UUID must not be set in header before testing certificate
-        # if req.requestHeaders.hasHeader('X-Device-UUID'):
-        #     req.requestHeaders.removeHeader('X-Device-UUID')
-        #     # TODO raise error?
-        #
-        # if req.requestHeaders.hasHeader('X-Participant-UUID'):
-        #     req.requestHeaders.removeHeader('X-Participant-UUID')
-        #     # TODO raise error ?
+        if req.requestHeaders.hasHeader('X-Device-UUID'):
+            req.requestHeaders.removeHeader('X-Device-UUID')
+            # TODO raise error?
+
+        if req.requestHeaders.hasHeader('X-Participant-UUID'):
+            req.requestHeaders.removeHeader('X-Participant-UUID')
+            # TODO raise error ?
         #
         # if cert is not None:
         #     # Certificate found, add information in header
