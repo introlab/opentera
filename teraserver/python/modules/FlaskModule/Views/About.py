f41dc8580af1efb748780af3bdde0613ec4e1edf
Dominic Létourneau <doumdi@gmail.com>
2020-10-22 14:32:48 -0400

    Refs #68, Displaying download information

--- teraserver/python/modules/FlaskModule/Views/About.py
+++ teraserver/python/modules/FlaskModule/Views/About.py
@@ -28,4 +28,5 @@ class About(MethodView):
         versions.load_from_db()
 
         return render_template('about.html', hostname=hostname, port=port,
-                               server_version=versions.version_string)
+                               server_version=versions.version_string,
+                               openteraplus_version=versions.get_client_version_with_name('OpenTeraPlus'))
