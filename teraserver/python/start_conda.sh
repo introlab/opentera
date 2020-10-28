68bf8ca74d2fb794e04aec4418b2b7cd88e5e900
Dominic Létourneau <doumdi@gmail.com>
2020-10-28 13:39:02 -0400

    Updated script

--- teraserver/python/start_conda.sh
+++ teraserver/python/start_conda.sh
@@ -2,4 +2,4 @@
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
 echo "$SCRIPTPATH"
-$SCRIPTPATH/env/python-3.6/bin/python3 $SCRIPTPATH/TeraServer.py
+$SCRIPTPATH/env/python-3.8/bin/python3 $SCRIPTPATH/TeraServer.py
