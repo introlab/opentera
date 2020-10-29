f1da446b75bce4504222a83c0ed30a2563b3c9a7
Dominic Létourneau <dominic.letourneau@usherbrooke.ca>
2020-10-28 21:24:04 -0400

    Removed useless prints

--- teraserver/python/tests/clients/RedisBenchmark.py
+++ teraserver/python/tests/clients/RedisBenchmark.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     config.load_config('../../config/TeraServerConfig.ini')
 
     start_time = datetime.now()
-
     rpc = RedisRPCClient(config.redis_config)
+
     count = 10000
     for i in range(count):
         status_users = rpc.call(ModuleNames.USER_MANAGER_MODULE_NAME.value, 'status_users')
