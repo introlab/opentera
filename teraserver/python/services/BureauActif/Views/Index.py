3d599ef91228ae93dcaad390e15a9c0bf4c0479a
cvilanova <cyvila@gmail.com>
2020-11-16 14:36:16 -0500

    Update Index.py

--- teraserver/python/services/BureauActif/Views/Index.py
+++ teraserver/python/services/BureauActif/Views/Index.py
@@ -12,27 +12,9 @@ class Index(MethodView):
         self.flaskModule = kwargs.get('flaskModule', None)
         print(self.flaskModule)
 
-    @AccessManager.token_required
     def get(self):
-        # print('get')
-        # print(request.cookies['BureauActifToken'])
 
-        hostname = self.flaskModule.config.server_config['hostname']
-        port = self.flaskModule.config.server_config['port']
-        backend_hostname = self.flaskModule.config.backend_config['hostname']
-        backend_port = self.flaskModule.config.backend_config['port']
-        if 'X_EXTERNALHOST' in request.headers:
-            backend_hostname = request.headers['X_EXTERNALHOST'];
-
-        if 'X_EXTERNALPORT' in request.headers:
-            backend_port = request.headers['X_EXTERNALPORT'];
-
-        # current_client.do_get_request_to_backend('/api/user/users?user_uuid=' + current_client.user_uuid)
-        # print(current_client.get_role_for_site(1))
-        # print(current_client.get_role_for_project(1))
-
-        return render_template('index.html', hostname=hostname, port=port,
-                               backend_hostname=backend_hostname, backend_port=backend_port)
+        return self.flaskModule.session.app.send_static_file('index.html')
 
     def post(self):
         print('post')
