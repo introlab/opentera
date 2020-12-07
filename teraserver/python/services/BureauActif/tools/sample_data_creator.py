ecab01d2b66439a4f613695bf32dedee25f4a868
cvilanova <cyvila@gmail.com>
2020-12-07 10:41:40 -0500

    Update sample_data_creator.py

--- teraserver/python/services/BureauActif/tools/sample_data_creator.py
+++ teraserver/python/services/BureauActif/tools/sample_data_creator.py
@@ -1,39 +1,59 @@
 import os
 from datetime import datetime, timedelta, timezone
 
+
+class Config:
+    seated = 15.00
+    standing = 30.00
+    up_time = 1200
+    down_time = 2400
+
+
 if __name__ == '__main__':
-    date = datetime(2020, 12, 6, 8, 0, 0, 0).astimezone()
+    config = Config()
     folder = r"C:\Users\vilc2301\Desktop\static"
+
+    with open(os.path.join(folder, 'Config.txt'), 'w') as config_file:
+        config_file.write(str(config.standing) + '\n')
+        config_file.write(str(config.seated) + '\n')
+
+    date = datetime(2020, 12, 5, 8, 0, 0, 0).astimezone()
     fileDay = 'Data_' + date.strftime("%Y-%m-%d") + '.dat'
+    fileTimers = 'Timers_' + date.strftime("%Y-%m-%d") + '.txt'
+    with open(os.path.join(folder, fileTimers), 'w') as config_file:
+        config_file.write(str(config.down_time) + '\n')
+        config_file.write(str(config.up_time) + '\n')
 
     counter = 0
     counter2 = 0
-    height = 15.00
-    expected = 15.00
+    timer = config.down_time
+    height = config.seated
+    expected = config.seated
     for i in range(14400):  # 4 hours of data
 
         # Change config (position expected)
-        if counter2 == 5400:
-            if expected == 15.00:
-                expected = 30.00
+        if counter2 == timer:
+            if expected == config.seated:
+                expected = config.standing
+                timer = config.up_time
                 counter2 = 0
             else:
-                expected = 15.00
+                expected = config.seated
+                timer = config.down_time
                 counter2 = 0
 
         # Change position
         if counter == 1800:
-            if height == 15.00:
-                height = 30.00
+            if height == config.seated:
+                height = config.standing
                 counter = 0
             else:
-                height = 15.00
+                height = config.seated
                 counter = 0
 
         if date.hour == 9 and 30 <= date.minute <= 59:
             pass
         else:
-
             line = str(date.isoformat()) + "\t" + str.format("{0:.2f}\t", height) + "--" + "\t" + str.format(
                 "{0:d}\t", 1) + "\t" + str.format("{0:.2f}", expected) + "\t" + str.format("{0:.2f}\t",
                                                                                            1.1) + "\t" + str.format(
