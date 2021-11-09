fff760c4b3c447cdaa01f402e2c971327825979c
Dominic Létourneau <doumdi@gmail.com>
2021-11-09 13:14:45 -0500

    Added query by id_device in UserQuerySessions and updated tests

--- teraserver/python/tests/modules/FlaskModule/API/user/test_UserQuerySessions.py
+++ teraserver/python/tests/modules/FlaskModule/API/user/test_UserQuerySessions.py
@@ -14,23 +14,23 @@ class UserQuerySessionsTest(BaseAPITest):
 
     def test_no_auth(self):
         response = self._request_with_no_auth()
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(401, response.status_code)
 
     def test_post_no_auth(self):
         response = self._post_with_no_auth()
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(401, response.status_code)
 
     def test_delete_no_auth(self):
         response = self._delete_with_no_auth(id_to_del=0)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(401, response.status_code)
 
     def test_query_no_params_as_admin(self):
         response = self._request_with_http_auth(username='admin', password='admin')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(400, response.status_code)
 
     def test_query_list_as_admin(self):
         response = self._request_with_http_auth(username='admin', password='admin', payload="id_session=1&list=1")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
         self.assertEqual(response.headers['Content-Type'], 'application/json')
         json_data = response.json()
         self.assertGreater(len(json_data), 0)
@@ -40,7 +40,7 @@ class UserQuerySessionsTest(BaseAPITest):
 
     def test_query_specific_as_admin(self):
         response = self._request_with_http_auth(username='admin', password='admin', payload="id_session=1")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
         self.assertEqual(response.headers['Content-Type'], 'application/json')
         json_data = response.json()
         self.assertEqual(len(json_data), 1)
@@ -48,23 +48,76 @@ class UserQuerySessionsTest(BaseAPITest):
         for data_item in json_data:
             self._checkJson(json_data=data_item)
 
+    def test_query_specific_but_invalid_as_admin(self):
+        response = self._request_with_http_auth(username='admin', password='admin', payload="id_session=-1")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        json_data = response.json()
+        self.assertEqual(len(json_data), 0)
+
     def test_query_for_participant_as_admin(self):
         response = self._request_with_http_auth(username='admin', password='admin', payload="id_participant=1")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
         self.assertEqual(response.headers['Content-Type'], 'application/json')
         json_data = response.json()
 
         for data_item in json_data:
             self._checkJson(json_data=data_item)
+            participant_count = 0
+            for participant in data_item['session_participants']:
+                participant_count += int(1 == participant['id_participant'])
+            self.assertEqual(1, participant_count)
+
+    def test_query_for_participant_as_admin_with_list(self):
+        response = self._request_with_http_auth(username='admin', password='admin', payload="id_participant=1&list=1")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        json_data = response.json()
+
+        for data_item in json_data:
+            self._checkJson(json_data=data_item, minimal=True)
 
     def test_query_for_user_as_admin(self):
         response = self._request_with_http_auth(username='admin', password='admin', payload="id_user=2")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
         self.assertEqual(response.headers['Content-Type'], 'application/json')
         json_data = response.json()
 
         for data_item in json_data:
             self._checkJson(json_data=data_item)
+            user_count = 0
+            for user in data_item['session_users']:
+                user_count += int(2 == user['id_user'])
+            self.assertEqual(1, user_count)
+
+    def test_query_for_user_as_admin_with_list(self):
+        response = self._request_with_http_auth(username='admin', password='admin', payload="id_user=2&list=1")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        json_data = response.json()
+
+        for data_item in json_data:
+            self._checkJson(json_data=data_item, minimal=True)
+
+    def test_query_for_device_as_admin(self):
+        response = self._request_with_http_auth(username='admin', password='admin', payload="id_device=2")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        json_data = response.json()
+        for data_item in json_data:
+            self._checkJson(json_data=data_item)
+            device_count = 0
+            for device in data_item['session_devices']:
+                device_count += int(2 == device['id_device'])
+            self.assertEqual(1, device_count)
+
+    def test_query_for_device_as_admin_with_list(self):
+        response = self._request_with_http_auth(username='admin', password='admin', payload="id_device=2&list=1")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        json_data = response.json()
+        for data_item in json_data:
+            self._checkJson(json_data=data_item, minimal=True)
 
     def test_post_and_delete(self):
         # New with minimal infos
@@ -78,19 +131,19 @@ class UserQuerySessionsTest(BaseAPITest):
         }
 
         response = self._post_with_http_auth(username='admin', password='admin', payload=json_data)
-        self.assertEqual(response.status_code, 400, msg="Missing id_session")  # Missing id_session
+        self.assertEqual(400, response.status_code, msg="Missing id_session")  # Missing id_session
 
         json_data['session']['id_session'] = 0
         response = self._post_with_http_auth(username='admin', password='admin', payload=json_data)
-        self.assertEqual(response.status_code, 400, msg="Missing participants")  # Missing participants
+        self.assertEqual(400, response.status_code, msg="Missing participants")  # Missing participants
 
         json_data['session']['session_participants_ids'] = [1, 2]
         response = self._post_with_http_auth(username='user4', password='user4', payload=json_data)
-        self.assertEqual(response.status_code, 403, msg="Post denied for user")  # Forbidden for that user to post that
+        self.assertEqual(403, response.status_code, msg="Post denied for user")  # Forbidden for that user to post that
 
         json_data['session']['session_users_ids'] = [1]
         response = self._post_with_http_auth(username='admin', password='admin', payload=json_data)
-        self.assertEqual(response.status_code, 200, msg="Post new")  # All ok now!
+        self.assertEqual(200, response.status_code, msg="Post new")  # All ok now!
 
         json_data = response.json()[0]
         self._checkJson(json_data)
@@ -105,7 +158,7 @@ class UserQuerySessionsTest(BaseAPITest):
         }
 
         response = self._post_with_http_auth(username='admin', password='admin', payload=json_data)
-        self.assertEqual(response.status_code, 200, msg="Post update")
+        self.assertEqual(200, response.status_code, msg="Post update")
         json_data = response.json()[0]
         self._checkJson(json_data)
         self.assertEqual(json_data['session_name'], 'Test Session 2')
@@ -119,7 +172,7 @@ class UserQuerySessionsTest(BaseAPITest):
             }
         }
         response = self._post_with_http_auth(username='admin', password='admin', payload=json_data)
-        self.assertEqual(response.status_code, 200, msg="Remove participants")
+        self.assertEqual(200, response.status_code, msg="Remove participants")
         json_data = response.json()[0]
         self._checkJson(json_data)
         self.assertEqual(len(json_data['session_participants']), 2)
@@ -127,10 +180,10 @@ class UserQuerySessionsTest(BaseAPITest):
         # self.assertEqual(json_data['session_participants'][1]['id_participant'], 3)
 
         response = self._delete_with_http_auth(username='user4', password='user4', id_to_del=current_id)
-        self.assertEqual(response.status_code, 403, msg="Delete denied")
+        self.assertEqual(403, response.status_code, msg="Delete denied")
 
         response = self._delete_with_http_auth(username='admin', password='admin', id_to_del=current_id)
-        self.assertEqual(response.status_code, 200, msg="Delete OK")
+        self.assertEqual(200, response.status_code, msg="Delete OK")
 
     def _checkJson(self, json_data, minimal=False):
         self.assertGreater(len(json_data), 0)
