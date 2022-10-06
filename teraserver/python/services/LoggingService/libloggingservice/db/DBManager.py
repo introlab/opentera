bd59495341d43da883fbbb1f542f694bb999d4e3
Simon Briere <simon.briere@usherbrooke.ca>
2022-10-06 10:55:25 -0400

    Refs #51. LoggingService: Added alembic in preparation to database changes

--- teraserver/python/services/LoggingService/libloggingservice/db/DBManager.py
+++ teraserver/python/services/LoggingService/libloggingservice/db/DBManager.py
@@ -1,5 +1,6 @@
 # Using same base as TeraServer
 from services.LoggingService.libloggingservice.db.Base import db
+from sqlalchemy.engine.reflection import Inspector
 
 # Must include all Database objects here to be properly initialized and created if needed
 # All at once to make sure all files are registered.
@@ -55,8 +56,17 @@ class DBManager:
         # db.drop_all()
         db.create_all()
 
-        # Apply any database upgrade, if needed
-        self.upgrade_db()
+        inspector = Inspector.from_engine(db.engine)
+        tables = inspector.get_table_names()
+        # tables = db.engine.table_names()
+        if not tables:
+            # Create all tables
+            db.create_all()
+            # New database - stamp with current revision version
+            self.stamp_db()
+        else:
+            # Apply any database upgrade, if needed
+            self.upgrade_db()
 
     def open_local(self, db_infos, echo=False):
         self.db_uri = 'sqlite:///%(filename)s' % db_infos
@@ -74,16 +84,66 @@ class DBManager:
         # db.drop_all()
         db.create_all()
 
-        # Apply any database upgrade, if needed
-        self.upgrade_db()
+        inspector = Inspector.from_engine(db.engine)
+        tables = inspector.get_table_names()
+        # tables = db.engine.table_names()
+        if not tables:
+            # Create all tables
+            db.create_all()
+            # New database - stamp with current revision version
+            self.stamp_db()
+        else:
+            # Apply any database upgrade, if needed
+            self.upgrade_db()
+
+    def init_alembic(self):
+        import sys
+        import os
+        # determine if application is a script file or frozen exe
+        if getattr(sys, 'frozen', False):
+            # If the application is run as a bundle, the pyInstaller bootloader
+            # extends the sys module by a flag frozen=True and sets the app
+            # path into variable _MEIPASS'.
+            this_file_directory = sys._MEIPASS
+            # When frozen, file directory = executable directory
+            root_directory = this_file_directory
+        else:
+            this_file_directory = os.path.dirname(os.path.abspath(__file__))
+            root_directory = os.path.join(this_file_directory, '..' + os.sep + '..')
+
+        # this_file_directory = os.path.dirname(os.path.abspath(inspect.stack()[0][1]))
+
+        alembic_directory = os.path.join(root_directory, 'alembic')
+        ini_path = os.path.join(root_directory, 'alembic.ini')
+
+        # create Alembic config and feed it with paths
+        config = Config(ini_path)
+        config.set_main_option('script_location', alembic_directory)
+        config.set_main_option('sqlalchemy.url', self.db_uri)
+
+        return config
 
     def upgrade_db(self):
-        # TODO ALEMBIC UPGRADES...
-        pass
+        config = self.init_alembic()
+
+        # prepare and run the command
+        revision = 'head'
+        sql = False
+        tag = None
+
+        # upgrade command
+        command.upgrade(config, revision, sql=sql, tag=tag)
 
     def stamp_db(self):
-        # TODO ALEMBIC UPGRADES
-        pass
+        config = self.init_alembic()
+
+        # prepare and run the command
+        revision = 'head'
+        sql = False
+        tag = None
+
+        # Stamp database
+        command.stamp(config, revision, sql, tag)
 
     def store_log_event(self, event: LogEvent):
         entry = LogEntry()
