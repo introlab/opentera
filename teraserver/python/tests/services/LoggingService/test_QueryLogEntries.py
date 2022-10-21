ed041b85f4f35cffd4232eedec33b0e8bf1251d7
Dominic Létourneau <doumdi@gmail.com>
2022-10-21 11:43:13 -0400

    Refs #51 #183 Base logging service API tests.

--- teraserver/python/tests/services/LoggingService/test_QueryLogEntries.py
+++ teraserver/python/tests/services/LoggingService/test_QueryLogEntries.py
@@ -31,7 +31,7 @@ class LoggingServiceDBManagerTest(BaseLoggingServiceAPITest):
             response = self._get_with_service_token_auth(self.test_client, token=token)
             self.assertEqual(response.status_code, 403)
 
-    def test_get_endpoint_with_valid_token_but_not_admin(self):
+    def test_get_endpoint_with_valid_token_and_admin(self):
         with flask_app.app_context():
             token = self._generate_fake_user_token(name='FakeUser', superadmin=True, expiration=3600)
             response = self._get_with_service_token_auth(self.test_client, token=token)
