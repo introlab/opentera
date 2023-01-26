0d555476117505b3cfeee4520ac3fa4747d2c5d5
Dominic Létourneau <doumdi@gmail.com>
2023-01-26 09:01:07 -0500

    Refs #191 Implemented disconnect for users and services, to be tested

--- teraserver/python/modules/FlaskModule/API/user/UserQueryDisconnect.py
+++ teraserver/python/modules/FlaskModule/API/user/UserQueryDisconnect.py
@@ -27,9 +27,9 @@ class UserQueryDisconnect(Resource):
         self.module = kwargs.get('flaskModule', None)
         self.test = kwargs.get('test', False)
 
-    @api.doc(description='Disconnect user/participant/device from server.',
+    @api.doc(description='Disconnect user/participant/device from server. Use Logout for current user instead.',
              responses={200: 'Success - user/participant/device will be disconnected.',
-                        400: 'No parameters specified at least one id / uuid must be used',
+                        400: 'No parameters specified, at least one id / uuid must be used',
                         403: 'Forbidden access. Please check that the user has access to'
                              ' the requested id/uuid.',
                         500: 'Database error'})
@@ -41,27 +41,31 @@ class UserQueryDisconnect(Resource):
 
         try:
             if args['id_user']:
-                if args['id_user'] in user_access.get_accessible_users_ids():
+                if args['id_user'] in user_access.get_accessible_users_ids(admin_only=True):
                     user = TeraUser.get_user_by_id(args['id_user'])
+                    if user.user_uuid == current_user.user_uuid:
+                        return gettext('Use Logout instead to disconnect current user'), 400
                     self.module.send_user_disconnect_module_message(user.user_uuid)
             elif args['user_uuid']:
-                if args['user_uuid'] in user_access.get_accessible_users_uuids():
+                if args['user_uuid'] in user_access.get_accessible_users_uuids(admin_only=True):
                     user = TeraUser.get_user_by_uuid(args['user_uuid'])
+                    if user.user_uuid == current_user.user_uuid:
+                        return gettext('Use Logout instead to disconnect current user'), 400
                     self.module.send_user_disconnect_module_message(user.user_uuid)
             elif args['id_participant']:
-                if args['id_participant'] in user_access.get_accessible_participants_ids():
+                if args['id_participant'] in user_access.get_accessible_participants_ids(admin_only=True):
                     participant = TeraParticipant.get_participant_by_id(args['id_participant'])
                     self.module.send_participant_disconnect_module_message(participant.participant_uuid)
             elif args['participant_uuid']:
-                if args['participant_uuid'] in user_access.get_accessible_participants_uuids():
+                if args['participant_uuid'] in user_access.get_accessible_participants_uuids(admin_only=True):
                     participant = TeraParticipant.get_participant_by_uuid(args['participant_uuid'])
                     self.module.send_participant_disconnect_module_message(participant.participant_uuid)
             elif args['id_device']:
-                if args['id_device'] in user_access.get_accessible_devices_ids():
+                if args['id_device'] in user_access.get_accessible_devices_ids(admin_only=True):
                     device = TeraDevice.get_device_by_id(args['id_device'])
                     self.module.send_device_disconnect_module_message(device.device_uuid)
             elif args['device_uuid']:
-                if args['device_uuid'] in user_access.get_accessible_devices_uuids():
+                if args['device_uuid'] in user_access.get_accessible_devices_uuids(admin_only=True):
                     device = TeraDevice.get_device_by_uuid(args['device_uuid'])
                     self.module.send_device_disconnect_module_message(device.device_uuid)
             else:
