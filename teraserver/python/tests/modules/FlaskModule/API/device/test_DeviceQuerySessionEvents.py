6fc9557065a6260c27eb3f33327600e8f6aa0570
Dominic Létourneau <doumdi@gmail.com>
2023-02-09 15:37:32 -0500

    Refs #194 fixed test.

--- teraserver/python/tests/modules/FlaskModule/API/device/test_DeviceQuerySessionEvents.py
+++ teraserver/python/tests/modules/FlaskModule/API/device/test_DeviceQuerySessionEvents.py
@@ -88,21 +88,26 @@ class DeviceQuerySessionEventsTest(BaseDeviceAPITest):
     def test_post_endpoint_with_valid_session_id_and_update_session_event(self):
         with self._flask_app.app_context():
             for device in TeraDevice.query.all():
-                for session in device.device_sessions:
-                    for event in session.session_events:
-                        schema = {'session_event': event.to_json(minimal=False)}
+                for session in TeraSession.query.all():
 
-                        # Update time
-                        schema['session_event']['session_event_datetime'] = str(datetime.now())
+                    schema = {'session_event': {'id_session': session.id_session,
+                                                'id_session_event_type': 1,
+                                                'id_session_event': 0,  # New!
+                                                'session_event_datetime': str(datetime.now())}
+                              }
 
-                        response = self._post_with_device_token_auth(self.test_client, token=device.device_token,
-                                                                     json=schema)
-                        if not device.device_enabled:
-                            self.assertEqual(response.status_code, 401)
-                            continue
+                    response = self._post_with_device_token_auth(self.test_client, token=device.device_token,
+                                                                 json=schema)
+                    if not device.device_enabled:
+                        self.assertEqual(401, response.status_code)
+                        continue
 
-                        # Test if the device owns the session, otherwise cannot add events
-                        if session.id_creator_device == device.id_device:
-                            self.assertEqual(response.status_code, 200)
-                        else:
-                            self.assertEqual(response.status_code, 403)
+                    session_devices = [dev.id_device for dev in session.session_devices]
+
+                    print('***403 ', device.id_device, session_devices)
+
+                    # Test if the device owns the session, otherwise cannot add events
+                    if device.id_device in session_devices:
+                        self.assertEqual(200, response.status_code)
+                    else:
+                        self.assertEqual(403, response.status_code)
