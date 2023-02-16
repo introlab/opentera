1e5c59b9bf72dd691301a5edb0ac1c5db3acf467
Dominic Létourneau <doumdi@gmail.com>
2023-02-16 12:14:16 -0500

    Ref #194, Updated with new test base class.

--- teraserver/python/tests/modules/FlaskModule/API/user/test_UserQueryVersions.py
+++ teraserver/python/tests/modules/FlaskModule/API/user/test_UserQueryVersions.py
@@ -1,42 +1,42 @@
-from tests.modules.FlaskModule.API.BaseAPITest import BaseAPITest
-
-import datetime
+from BaseUserAPITest import BaseUserAPITest
 
 
-class UserQueryVersionsTest(BaseAPITest):
-    login_endpoint = '/api/user/login'
+class UserQueryVersionsTest(BaseUserAPITest):
     test_endpoint = '/api/user/versions'
 
     def setUp(self):
-        pass
+        super().setUp()
 
     def tearDown(self):
-        pass
+        super().tearDown()
 
     def test_no_auth(self):
-        response = self._request_with_no_auth()
-        self.assertEqual(response.status_code, 401)
+        with self._flask_app.app_context():
+            response = self._get_with_user_http_auth(self.test_client)
+            self.assertEqual(401, response.status_code)
 
     def test_post_no_auth(self):
-        response = self._post_with_no_auth()
-        self.assertEqual(response.status_code, 401)
+        with self._flask_app.app_context():
+            response = self._post_with_user_http_auth(self.test_client, username='', password='')
+            self.assertEqual(401, response.status_code)
 
     def test_query_get_no_params_as_admin(self):
-        response = self._request_with_http_auth(username='admin', password='admin')
-        self.assertEqual(response.status_code, 200)
-        value = response.json()
-        self.assertTrue('version_string' in value)
-        self.assertTrue('version_major' in value)
-        self.assertTrue('version_minor' in value)
-        self.assertTrue('version_patch' in value)
-        self.assertTrue('clients' in value)
+        with self._flask_app.app_context():
+            response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin')
+            self.assertEqual(200, response.status_code)
+            value = response.json
+            self.assertTrue('version_string' in value)
+            self.assertTrue('version_major' in value)
+            self.assertTrue('version_minor' in value)
+            self.assertTrue('version_patch' in value)
+            self.assertTrue('clients' in value)
 
-        # Test for clients
-        for client_name in value['clients']:
-            client = value['clients'][client_name]
-            self.assertTrue('client_name' in client)
-            self.assertTrue('client_description' in client)
-            self.assertTrue('client_version' in client)
-            self.assertTrue('client_windows_download_url' in client)
-            self.assertTrue('client_mac_download_url' in client)
-            self.assertTrue('client_linux_download_url' in client)
+            # Test for clients
+            for client_name in value['clients']:
+                client = value['clients'][client_name]
+                self.assertTrue('client_name' in client)
+                self.assertTrue('client_description' in client)
+                self.assertTrue('client_version' in client)
+                self.assertTrue('client_windows_download_url' in client)
+                self.assertTrue('client_mac_download_url' in client)
+                self.assertTrue('client_linux_download_url' in client)
--- teraserver/python/tests/modules/FlaskModule/API/user/test_UserRefreshToken.py
+++ teraserver/python/tests/modules/FlaskModule/API/user/test_UserRefreshToken.py
@@ -12,47 +12,51 @@ class UserRefreshTokenTest(BaseUserAPITest):
         super().tearDown()
 
     def test_no_token_http_auth_refresh(self):
-        response = self._get_with_user_token_auth(self.test_client, token='')
-        self.assertEqual(401, response.status_code)
+        with self._flask_app.app_context():
+            response = self._get_with_user_token_auth(self.test_client, token='')
+            self.assertEqual(401, response.status_code)
 
     def test_valid_token_refresh(self):
-        response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin', 
-                                                 endpoint=self.login_endpoint)
-        self.assertEqual(200, response.status_code)
-        login_info = response.json
-        self.assertTrue(login_info.__contains__('user_token'))
-        token = login_info['user_token']
-        response = self._get_with_user_token_auth(self.test_client, token=token)
-        self.assertEqual(200, response.status_code)
-        token_info = response.json
-        self.assertTrue(token_info.__contains__('refresh_token'))
-        refresh_token = token_info['refresh_token']
-        self.assertGreater(len(refresh_token), 0)
+        with self._flask_app.app_context():
+            response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin',
+                                                     endpoint=self.login_endpoint)
+            self.assertEqual(200, response.status_code)
+            login_info = response.json
+            self.assertTrue(login_info.__contains__('user_token'))
+            token = login_info['user_token']
+            response = self._get_with_user_token_auth(self.test_client, token=token)
+            self.assertEqual(200, response.status_code)
+            token_info = response.json
+            self.assertTrue(token_info.__contains__('refresh_token'))
+            refresh_token = token_info['refresh_token']
+            self.assertGreater(len(refresh_token), 0)
 
     def test_invalid_token_refresh_with_disabled_token(self):
-        response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin',
-                                                 endpoint=self.login_endpoint)
-        self.assertEqual(200, response.status_code)
-        login_info = response.json
-        self.assertTrue(login_info.__contains__('user_token'))
-        login_token = login_info['user_token']
-        # Refresh token
-        response = self._get_with_user_token_auth(self.test_client, token=login_token)
-        self.assertEqual(200, response.status_code)
-        token_info = response.json
-        self.assertTrue(token_info.__contains__('refresh_token'))
-        refresh_token = token_info['refresh_token']
-        self.assertGreater(len(refresh_token), 0)
-        # Login should not work, token should be disabled
-        response = self._get_with_user_token_auth(self.test_client, token=login_token, endpoint=self.login_endpoint)
-        self.assertEqual(401, response.status_code)
+        with self._flask_app.app_context():
+            response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin',
+                                                     endpoint=self.login_endpoint)
+            self.assertEqual(200, response.status_code)
+            login_info = response.json
+            self.assertTrue(login_info.__contains__('user_token'))
+            login_token = login_info['user_token']
+            # Refresh token
+            response = self._get_with_user_token_auth(self.test_client, token=login_token)
+            self.assertEqual(200, response.status_code)
+            token_info = response.json
+            self.assertTrue(token_info.__contains__('refresh_token'))
+            refresh_token = token_info['refresh_token']
+            self.assertGreater(len(refresh_token), 0)
+            # Login should not work, token should be disabled
+            response = self._get_with_user_token_auth(self.test_client, token=login_token, endpoint=self.login_endpoint)
+            self.assertEqual(401, response.status_code)
 
     def test_invalid_token_refresh_with_no_token(self):
-        response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin',
-                                                 endpoint=self.login_endpoint)
-        self.assertEqual(200, response.status_code)
-        login_info = response.json
-        self.assertTrue(login_info.__contains__('user_token'))
-        login_token = login_info['user_token']
-        response = self._get_with_user_token_auth(self.test_client, token='')
-        self.assertEqual(401, response.status_code)
+        with self._flask_app.app_context():
+            response = self._get_with_user_http_auth(self.test_client, username='admin', password='admin',
+                                                     endpoint=self.login_endpoint)
+            self.assertEqual(200, response.status_code)
+            login_info = response.json
+            self.assertTrue(login_info.__contains__('user_token'))
+            login_token = login_info['user_token']
+            response = self._get_with_user_token_auth(self.test_client, token='')
+            self.assertEqual(401, response.status_code)
