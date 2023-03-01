45092947be78b6865591d0cd64faf322a04ea9e6
Dominic Létourneau <doumdi@gmail.com>
2023-03-01 08:48:27 -0500

    fix python version for manual package

--- teraserver/python/package.sh
+++ teraserver/python/package.sh
@@ -1,2 +1,2 @@
-./env/python-3.8/bin/python3 -m pip install --upgrade setuptools wheel twine
-./env/python-3.8/bin/python3 setup.py sdist bdist_wheel
+./env/python-3.10/bin/python3 -m pip install --upgrade setuptools wheel twine
+./env/python-3.10/bin/python3 setup.py sdist bdist_wheel
