5496b8ae6f7e0d6bde42e7e82d33f0079e91862e
Simon Briere <simon.briere@usherbrooke.ca>
2023-03-21 13:54:37 -0400

    Refs #204. Updated API docs.

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': -1
+}
+
 html_static_path = ['_static']
 
 source_suffix = {
