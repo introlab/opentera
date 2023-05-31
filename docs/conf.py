77f8b750bf5e02d546ef168122f39dffc492f17c
Simon Briere <simon.briere@usherbrooke.ca>
2023-05-31 11:42:43 -0400

    Updated version to 1.2.4

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'OpenTera'
 copyright = '2023, Simon Brière, Dominic Létourneau'
 author = 'Simon Brière, Dominic Létourneau'
-release = '1.2.3'
+release = '1.2.4'
 version = release
 
 html_logo = 'images/LogoOpenTera200px.png'
