2234df70cb2637831801586b291e0689f81d7ed9
Dominic Létourneau <doumdi@gmail.com>
2024-11-26 09:26:31 -0500

    Updated test structure

--- teraserver/python/tests/modules/DatabaseModule/test_DBManagerTeraDeviceAccess.py
+++ teraserver/python/tests/modules/DatabaseModule/test_DBManagerTeraDeviceAccess.py
@@ -12,5 +12,79 @@ from tests.opentera.db.models.BaseModelsTest import BaseModelsTest
 
 
 class DBManagerTeraDeviceAccessTest(BaseModelsTest):
-    # TODO - Implement tests
-    pass
+    """
+    Tests for the DBManagerTeraDeviceAccess class.
+    """
+
+    def test_device_query_sesion(self):
+        """
+        Query session for a device with id.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                # Query invalid session
+                session = device_access.query_session(session_id=0)
+                self.assertIsNone(session)
+
+    def test_device_query_existing_session(self):
+        """
+        Query session for a device with existing session.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                pass
+
+
+    def test_device_get_accessible_sessions_ids(self):
+        """
+        Get accessible sessions for a device.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                pass
+
+    def test_device_get_accessible_session_types_ids(self):
+        """
+        Get accessible session types for a device.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                pass
+
+    def test_device_get_accessible_assets(self):
+        """
+        Get accessible assets for a device.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                pass
+
+    def test_device_get_accessible_services(self):
+        """
+        Get accessible services for a device.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                pass
+
+    def test_device_get_accessible_tests_types_ids(self):
+        """
+        Get accessible tests types for a device.
+        """
+        with self._flask_app.app_context():
+            devices = TeraDevice.query.all()
+            for device in devices:
+                device_access : DBManagerTeraDeviceAccess = DBManager.deviceAccess(device)
+                pass
