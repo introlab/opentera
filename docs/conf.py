4e36c7893cbcca0487eb22763f4931e06cecb4a6
Simon Briere <simon.briere@usherbrooke.ca>
2026-02-18 14:57:09 -0500

    Fix to regenerate doc.

--- docs/conf.py
+++ docs/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'OpenTera'
-copyright = '2025, Simon Brière, Dominic Létourneau'
+copyright = '2026, Simon Brière, Dominic Létourneau'
 author = 'Simon Brière, Dominic Létourneau'
-release = '1.3.2'
+release = '1.3.1'
 version = release
 
 html_logo = 'images/LogoOpenTera200px.png'
